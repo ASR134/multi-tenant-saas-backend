7a6332799ddb565250edfef7aaacfea79dd73263
Aman Singh Rawat <amansinghrawat992752@gmail.com>
2026-09-25 21:09:12 +0530

    fix: update cors

--- app/main.py
+++ app/main.py
@@ -26,7 +26,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://www.stackwork.dev",
-        "https://stackwork.dev",
         "https://multi-tenant-saas-frontend-lovat.vercel.app",  # deployed Vercel URL
         "http://localhost:5173",             # Vite's default local dev port
     ],
